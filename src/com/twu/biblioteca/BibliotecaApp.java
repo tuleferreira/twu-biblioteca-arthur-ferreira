0e655ee962300c56e6597ba180023423bfecb84d
tuleferreira <arthur.j.ferreira@gmail.com>
2019-07-18 15:30:14 -0300

    [1.1] View a welcome message

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -2,7 +2,11 @@ package com.twu.biblioteca;
 
 public class BibliotecaApp {
 
-    public static void main(String[] args) {
-        System.out.println();
+    public static void main() {
+        sayHello();
+    }
+
+    private static void sayHello() {
+        System.out.println("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore");
     }
 }
--- test/com/twu/biblioteca/BibliotecaAppTest.java
+++ test/com/twu/biblioteca/BibliotecaAppTest.java
@@ -3,6 +3,9 @@ package com.twu.biblioteca;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.Assert.*;
 
 public class BibliotecaAppTest {
@@ -13,5 +16,10 @@ public class BibliotecaAppTest {
 
     @Test
     public void main() {
+        final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        BibliotecaApp.main();
+        assertEquals("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore\n", outContent.toString());
+        System.setOut(null);
     }
 }
